7400497bdabf7918c1ccc8c2cfa98cf81d6f497d
CalebCalnek <calebgcc156@gmail.com>
2026-04-25 13:45:48 -0600

    move shared string conversion and produce behaviors to superclasses

--- cfg.py
+++ cfg.py
@@ -2,15 +2,25 @@ import random
 
 class Symbol():
 	def is_terminal(self):
+		""" evaluate whether the symbol is a terminal
+		"""
 		pass
 
 class Terminal(Symbol):
+	def __str__(self):
+		return self.word
 	def is_terminal(self):
 		return True
 
 class Production(Symbol):
+	def __str__(self):
+		symbols = [str(i) for i in self.symbols]
+		symbol_string = " ".join(symbols)
+		return symbol_string
 	def produce(self):
-		pass
+		for symbol in self.symbols:
+			if not symbol.is_terminal():
+				symbol.produce()
 	def is_terminal(self):
 		return False
 
@@ -22,32 +32,24 @@ class Sentence(Production):
 		symbol_string = " ".join(symbols)
 		sentence_string = f"{symbol_string}."
 		return sentence_string
-	def produce(self):
-		while not all([symbol.is_terminal() for symbol in self.symbols]):
-			for i in range(len(self.symbols)):
-				self.symbols[i] = self.symbols[i].produce()
 
 class NounPhrase(Production):
-	def produce(self):
-		return Noun()
+	def __init__(self):
+		self.symbols = [Noun()]
 
 class VerbPhrase(Production):
-	def produce(self):
-		return Verb()
+	def __init__(self):
+		self.symbols = [Verb()]
 
 class Noun(Terminal):
 	nouns = ("dog", "I", "John")
 	def __init__(self):
 		self.word = random.choice(self.nouns)
-	def __str__(self):
-		return self.word
 
 class Verb(Terminal):
 	verbs = ("be", "have", "go")
 	def __init__(self):
 		self.word = random.choice(self.verbs)
-	def __str__(self):
-		return self.word
 
 sentence = Sentence()
 sentence.produce()
