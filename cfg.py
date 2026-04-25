6165f438bf15c745c67248452db6f1715c02db33
CalebCalnek <calebgcc156@gmail.com>
2026-04-25 13:53:34 -0600

    remove unused is_terminal and produce methods

--- cfg.py
+++ cfg.py
@@ -1,28 +1,17 @@
 import random
 
 class Symbol():
-	def is_terminal(self):
-		""" evaluate whether the symbol is a terminal
-		"""
-		pass
+	pass
 
 class Terminal(Symbol):
 	def __str__(self):
 		return self.word
-	def is_terminal(self):
-		return True
 
 class Production(Symbol):
 	def __str__(self):
 		symbols = [str(i) for i in self.symbols]
 		symbol_string = " ".join(symbols)
 		return symbol_string
-	def produce(self):
-		for symbol in self.symbols:
-			if not symbol.is_terminal():
-				symbol.produce()
-	def is_terminal(self):
-		return False
 
 class Sentence(Production):
 	def __init__(self):
@@ -52,6 +41,5 @@ class Verb(Terminal):
 		self.word = random.choice(self.verbs)
 
 sentence = Sentence()
-sentence.produce()
 print(sentence)
 
