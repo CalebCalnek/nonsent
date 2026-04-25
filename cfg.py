8e8ad7d06513db0ab2f6fc01ffb96dab3c6dafb8
CalebCalnek <calebgcc156@gmail.com>
2026-04-25 14:20:38 -0600

    add adjective class

--- cfg.py
+++ cfg.py
@@ -29,11 +29,22 @@ class NounPhrase(Production):
 		self.symbols = []
 		if random.random() > 0.5:
 			self.symbols.append(Determiner())
-		self.symbols.append(Noun())
+		if random.random() > 0.5:
+			self.symbols.append(AdjectivePhrase())
+		if random.random() > 0.5:
+			self.symbols.append(Noun())
+		else:
+			self.symbols.append(NounPhrase())
 
 class VerbPhrase(Production):
 	def __init__(self):
-		self.symbols = [Verb()]
+		self.symbols = []
+		self.symbols.append(Verb())
+
+class AdjectivePhrase(Production):
+	def __init__(self):
+		self.symbols = []
+		self.symbols.append(Adjective())
 
 # Terminals
 
@@ -46,6 +57,11 @@ class Verb(Terminal):
 	verbs = ("be", "have", "go", "let", "get", "do", "take", "make", "put")
 	def __init__(self):
 		self.word = random.choice(self.verbs)
+
+class Adjective(Terminal):
+	adjectives = ("other", "new", "good", "old", "different", "local", "great", "small")
+	def __init__(self):
+		self.word = random.choice(self.adjectives)
 
 class Determiner(Terminal):
 	determiners = ("the", "a", "that", "this", "which", "whose", "his", "their", "her")
