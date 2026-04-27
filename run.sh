1487497cfbffcd5be41f2cdb5ef5a00f009fe735
CalebCalnek <calebgcc156@gmail.com>
2026-04-26 21:53:51 -0600

    add espeak option to run script

--- run.sh
+++ run.sh
@@ -4,7 +4,11 @@ printf '\e[?1049h'
 clear
 
 while true; do
-	python cfg.py
+	if [[ $@ == "--espeak" ]]; then
+		python cfg.py | tee >(espeak-ng -v en-us+announcer -s 150)
+	else
+		python cfg.py
+	fi
 	read -rsn1 input
 	case $input in
 		c) clear ;;
@@ -14,4 +18,3 @@ while true; do
 done
 
 printf '\e[?1049l'
-
