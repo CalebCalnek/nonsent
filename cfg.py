4bbb47d2b4eaed9a906d9ad648cf231570ea3972
CalebCalnek <calebgcc156@gmail.com>
2026-04-26 22:29:53 -0600

    support for setting execution count for less file load overhead in scripts

--- cfg.py
+++ cfg.py
@@ -1,5 +1,18 @@
+import sys
 from productions import Sentence
 
-sentence = Sentence()
-print(sentence)
+exec_count = 1
+
+if len(sys.argv) > 1 and sys.argv[1] == "--help":
+	print("usage: python cfg.py [-n exec_count]")
+	exit(1)
+
+for i in range(1, len(sys.argv)):
+	if sys.argv[i] == "-n":
+		i += 1
+		exec_count = int(sys.argv[i])
+
+for i in range(exec_count):
+	sentence = Sentence()
+	print(sentence)
 
